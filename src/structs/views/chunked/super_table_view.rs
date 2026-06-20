5c8cac588cb7b8ebbe418f353156612b15e41006
PB <37089506+pbower@users.noreply.github.com>
2026-06-20 06:04:15 +0100

    Update copyright year range

--- src/structs/views/chunked/super_table_view.rs
+++ src/structs/views/chunked/super_table_view.rs
@@ -1,4 +1,4 @@
-// Copyright 2025 Peter Garfield Bower
+// Copyright 2025-2026 Peter Garfield Bower. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
